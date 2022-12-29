59a2088f7ca9826c58fbeda9a6c809a3fadebf23
abuyusr <abdizm@outlook.com>
2022-12-28 22:37:56 -0500

    If applied , this will define the biggest Pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
                    {name:'Blastoise', type:'water', height:5 }    
                    ]
  // Used document.write() inside the loop’s code
+ // Added heigth and updated the conditions.
+for (let i=0; i< pokemonList.length;i++) {
+  if (pokemonList[i].height >= 7) {
+      document.write (pokemonList[i].name + " (height:" + pokemonList[i].height + "m) - Wow, that is big!" + "<br>")
+  } else if (pokemonList[i].height >= 4 && pokemonList[i].height < 6) {
+      document.write (pokemonList[i].name + " (height:" + pokemonList[i].height + " m ) - This is medium" + "<br>")
+  } else {
+      document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + " m ) - This is small Pokemon "+" <br>")
+  }
+}
 
 
 
