c60f2306eeadc23ea66d357a2b9ebe625bef804d
abuyusr <abdizm@outlook.com>
2022-12-28 22:27:12 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,6 @@ let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
                    {name:'Blastoise', type:'water', height:5 }    
                    ]
  // Used document.write() inside the loop’s code
-for (let i=0; i < pokemonList.length;i++) {
-    if (pokemonList[i].height <5 && pokemonList[i].height >3) {
-        document.write (pokemonList[i].name + " This is medium");}
-    else if (pokemonList[i].height < 3 ) { document.write(pokemonList[i].name + " This is small");}
-    else { document.write(pokemonList[1].name + "  Wow, that is big! ")}
 
-}
+
 
