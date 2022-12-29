7c06dfaa3154cf7962a393b686965ca66c9e9440
abuyusr <abdizm@outlook.com>
2022-12-28 21:41:45 -0500

    if applied, this updates for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 alert('Hello world');
 // added new pokomen variables
-let pokemonList =[ {name:"Bulbasaur", type:"grass", height:"3"},
-{name:"Ivysaur", type:"poison", height:"7"},
-{name:"Blastoise", type:"water", height:"5"}    
-]
+let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
+                   {name:'Ivysaur', type:'poison', height:7 },
+                   {name:'Blastoise', type:'water', height:5 }    
+                   ]
  // Used document.write() inside the loop’s code
 for (let i=0; i < pokemonList.length;i++) {
     if (pokemonList[i].height <5 && pokemonList[i].height >3) {
-        document.write (pokemonList[i].name +" This is medium");
-    }  else if (pokemonList[i].height < 3 ) { document.write(pokemonList[i].name + "This is small");}
-    else { document.write(pokemonList[1].name + " Wow, that is big! ")}
+        document.write (pokemonList[i].name + " This is medium");}
+    else if (pokemonList[i].height < 3 ) { document.write(pokemonList[i].name + " This is small");}
+    else { document.write(pokemonList[1].name + "  Wow, that is big! ")}
 
 }
 
