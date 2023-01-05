7b295e177d72ec3777aa816d32ea3b079be00e8c
abuyusr <abdizm@outlook.com>
2023-01-05 08:13:57 -0500

    if applied this will update forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
                    ]
  // Used document.write() inside the loop’s code
  // Added heigth and updated the conditions.
-for (let i=0; i< pokemonList.length;i++) {
+/* for (let i=0; i< pokemonList.length;i++) {
   if (pokemonList[i].height >= 7) {
       document.write (pokemonList[i].name + " (height:" + pokemonList[i].height + "m) - Wow, that is big!" + "<br>")
   } else if (pokemonList[i].height >= 4 && pokemonList[i].height < 6) {
@@ -14,7 +14,32 @@ for (let i=0; i< pokemonList.length;i++) {
   } else {
       document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + " m ) - This is small Pokemon "+" <br>")
   }
-}
+} */
 
+// wrapped  pokemonList array in an IIFE 
+let pokemonRepository =(function(){
+    let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
+                     {name:'Ivysaur', type:'poison', height:7 },
+                     {name:'Blastoise', type:'water', height:5 },    
+                     ];
+    
+    function add(pokemon) {
+      pokemonList (pokemon);
+     }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+     return {
+      add: add,
+      getAll: getAll
+     };
+   })();
+  
+   // used forEach loop to retrieve the pokemonList array.
+      pokemonRepository.getAll().forEach(function (pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.type + ' type wiht height ' + pokemon.height + ' m'    );
+  });
 
 
